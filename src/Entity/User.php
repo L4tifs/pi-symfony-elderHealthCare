6f91f72f9408ca224948e4778fce87d6ead3eed8
zeinebsol <zeinebsoltani9@gmail.com>
2026-02-04 21:54:59 +0100

    fix bd

---
+++ src/Entity/User.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 33)]
+    private ?string $idUser = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $no = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdUser(): ?string
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser(string $idUser): static
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNo(): ?string
+    {
+        return $this->no;
+    }
+
+    public function setNo(string $no): static
+    {
+        $this->no = $no;
+
+        return $this;
+    }
+}
